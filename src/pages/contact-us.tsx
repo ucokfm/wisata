85cdcd02cb082e45d5b171be072756abe8e39038
Ucok Freddy <ucokfm@gmail.com>
2019-08-19 15:58:23 +0700

    feat: show form in contact-us

--- src/pages/contact-us.tsx
+++ src/pages/contact-us.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Formik } from 'formik';
 
 interface Props {}
 
@@ -7,6 +8,27 @@ export class ContactUs extends React.Component<Props> {
     return (
       <div>
         <h1>Contact Us</h1>
+        <Formik
+          initialValues={{ name: '', email: '', message: '' }}
+          onSubmit={(values) => {
+            console.log({ values });
+          }}
+        >
+          {({ values, handleChange, handleBlur, handleSubmit }) => (
+            <form onSubmit={handleSubmit}>
+              <div>
+                Nama: <input name="name" onChange={handleChange} onBlur={handleBlur} value={values.name} />
+              </div>
+              <div>
+                Email: <input name="email" onChange={handleChange} onBlur={handleBlur} value={values.email} />
+              </div>
+              <div>
+                Pesan: <textarea name="message" onChange={handleChange} onBlur={handleBlur} value={values.message} />
+              </div>
+              <button type="submit">Submit</button>
+            </form>
+          )}
+        </Formik>
       </div>
     );
   }
