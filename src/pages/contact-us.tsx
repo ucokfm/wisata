ad07683baf85a57a2471603ba8dbab0014003cf1
Ucok Freddy <ucokfm@gmail.com>
2019-08-19 17:23:28 +0700

    feat: use semantic ui

--- src/pages/contact-us.tsx
+++ src/pages/contact-us.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Formik } from 'formik';
 import { observer } from 'mobx-react';
+import { Button, Form, Input } from 'semantic-ui-react';
 
 import { appStore } from '../store';
 
@@ -10,7 +11,7 @@ interface Props {}
 export class ContactUs extends React.Component<Props> {
   render() {
     return (
-      <div>
+      <div style={{ padding: 30 }}>
         <h1>Contact Us</h1>
         <Formik
           initialValues={{ name: '', email: '', message: '' }}
@@ -20,18 +21,20 @@ export class ContactUs extends React.Component<Props> {
           }}
         >
           {({ values, handleChange, handleBlur, handleSubmit, resetForm }) => (
-            <form onSubmit={handleSubmit}>
-              <div>
-                Nama: <input name="name" onChange={handleChange} onBlur={handleBlur} value={values.name} />
-              </div>
-              <div>
-                Email: <input name="email" onChange={handleChange} onBlur={handleBlur} value={values.email} />
-              </div>
-              <div>
-                Pesan: <textarea name="message" onChange={handleChange} onBlur={handleBlur} value={values.message} />
-              </div>
-              <button type="submit">Submit</button>
-            </form>
+            <Form onSubmit={handleSubmit}>
+              <Form.Field>
+                <label>Nama</label>
+                <Input name="name" onChange={handleChange} onBlur={handleBlur} value={values.name} />
+              </Form.Field>
+              <Form.Field>
+                <label>Email</label>
+                <Input name="email" onChange={handleChange} onBlur={handleBlur} value={values.email} />
+              </Form.Field>
+              <Form.Field>
+                Pesan: <Input name="message" onChange={handleChange} onBlur={handleBlur} value={values.message} />
+              </Form.Field>
+              <Button type="submit" primary>Submit</Button>
+            </Form>
           )}
         </Formik>
       </div>
