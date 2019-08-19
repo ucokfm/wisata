563c1e8b6a38b3bd73eeb0d3621a48b0a1806cdf
Ucok Freddy <ucokfm@gmail.com>
2019-08-19 17:07:27 +0700

    feat: set dummy data for cities

--- src/pages/detail.tsx
+++ src/pages/detail.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import { observer } from 'mobx-react';
+
+import { appStore } from '../store';
 
 interface Props {}
 
+@observer
 class BaseDetail extends React.Component<Props> {
+  state = {
+    city: 'denpasar',
+  };
+
   async componentDidMount() {
     const city: string = (this.props as any).match.params.city;
-    console.log({ city });
-
+    // console.log({ city });
+    this.setState({ city });
   }
 
   render() {
     const city: string = (this.props as any).match.params.city;
+    const data = (appStore as any).cities[city];
     return (
       <div>
         <h1>Detail {city}</h1>
         <ul>
-          <li>Kisaran paket wisata per hari: </li>
-          <li>Jumlah kunjungan per tahun: </li>
+          <li>Kisaran paket wisata per hari: Rp{data.price}</li>
+          <li>Jumlah kunjungan per tahun: {data.count}</li>
         </ul>
       </div>
     );
--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,29 @@
 import { observable } from 'mobx';
 
 class AppStore {
+  @observable cities = {
+    denpasar: {
+      price: 1000000,
+      count: 3500,
+    },
+    medan: {
+      price: 500000,
+      count: 1200,
+    },
+    jakarta: {
+      price: 750000,
+      count: 5000,
+    },
+    bandung: {
+      price: 650000,
+      count: 2500,
+    },
+    surabaya: {
+      price: 350000,
+      count: 800,
+    },
+  }
+
   @observable guests = [
     { name: 'budi', message: 'hey' }
   ];
