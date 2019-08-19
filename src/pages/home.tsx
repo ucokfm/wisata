3c2d9ea41796c1c2d0677f6496b8333936139464
Ucok Freddy <ucokfm@gmail.com>
2019-08-19 18:03:16 +0700

    feat: show weather status text

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,11 +7,11 @@ interface Props {}
 
 export class Home extends React.Component<Props> {
   state = {
-    denpasar: '0',
-    medan: '0',
-    jakarta: '0',
-    bandung: '0',
-    surabaya: '0',
+    denpasar: { t: '0', w: '' },
+    medan: { t: '0', w: '' },
+    jakarta: { t: '0', w: '' },
+    bandung: { t: '0', w: '' },
+    surabaya: { t: '0', w: '' },
   }
 
   async componentDidMount() {
@@ -24,21 +24,43 @@ export class Home extends React.Component<Props> {
     };
     for (const [k, v] of Object.entries(urls)) {
       const jsonData = xml2js((await axios.get(v) as any).data);
-      console.log({ [k]: jsonData });
+      // console.log({ [k]: jsonData });
       let t = '';
+      let w = '';
       if (k === 'denpasar') {
         t = jsonData["elements"][0]["elements"][0]["elements"][3]["elements"][7]["elements"][0]["elements"][0]["elements"][0].text; 
+        w = jsonData["elements"][0]["elements"][0]["elements"][3]["elements"][8]["elements"][0]["elements"][0]["elements"][0].text; 
       } else if (k === 'medan') {
         t = jsonData["elements"][0]["elements"][0]["elements"][14]["elements"][7]["elements"][0]["elements"][0]["elements"][0].text; 
+        w = jsonData["elements"][0]["elements"][0]["elements"][3]["elements"][8]["elements"][0]["elements"][0]["elements"][0].text; 
       } else if (k === 'jakarta') {
         t = jsonData["elements"][0]["elements"][0]["elements"][2]["elements"][7]["elements"][0]["elements"][0]["elements"][0].text; 
+        w = jsonData["elements"][0]["elements"][0]["elements"][3]["elements"][8]["elements"][0]["elements"][0]["elements"][0].text; 
       } else if (k === 'bandung') {
         t = jsonData["elements"][0]["elements"][0]["elements"][1]["elements"][7]["elements"][0]["elements"][0]["elements"][0].text; 
+        w = jsonData["elements"][0]["elements"][0]["elements"][3]["elements"][8]["elements"][0]["elements"][0]["elements"][0].text; 
       } else if (k === 'surabaya') {
         t = jsonData["elements"][0]["elements"][0]["elements"][35]["elements"][7]["elements"][0]["elements"][0]["elements"][0].text; 
+        w = jsonData["elements"][0]["elements"][0]["elements"][3]["elements"][8]["elements"][0]["elements"][0]["elements"][0].text; 
       }
-      this.setState({ [k]: t });
+      this.setState({ [k]: { t, w } });
     }
+  }
+
+  formatWeather(w: any) {
+    let status = '';
+    if (w === '100') {
+      status = 'Cerah';
+    } else if (w === '101') {
+      status = 'Cerah Berawan';
+    } else if (w === '102') {
+      status = 'Cerah Berawan';
+    } else if (w === '103') {
+      status = 'Berawan';
+    } else if (w === '104') {
+      status = 'Berawan';
+    }
+    return status;
   }
 
   render() {
@@ -46,11 +68,11 @@ export class Home extends React.Component<Props> {
       <div style={{ padding: 30 }}>
         <h1>Home</h1>
         <ol>
-          <li><Link to="/detail/denpasar">Denpasar: </Link>{this.state.denpasar} C</li>
-          <li><Link to="/detail/medan">Medan: </Link>{this.state.medan} C</li>
-          <li><Link to="/detail/jakarta">Jakarta: </Link>{this.state.jakarta} C</li>
-          <li><Link to="/detail/bandung">Bandung: </Link>{this.state.bandung} C</li>
-          <li><Link to="/detail/surabaya">Surabaya: </Link>{this.state.surabaya} C</li>
+          <li><Link to="/detail/denpasar">Denpasar: </Link>{this.state.denpasar.t} C ({this.formatWeather(this.state.denpasar.w)})</li>
+          <li><Link to="/detail/medan">Medan: </Link>{this.state.medan.t} C ({this.formatWeather(this.state.medan.w)})</li>
+          <li><Link to="/detail/jakarta">Jakarta: </Link>{this.state.jakarta.t} C ({this.formatWeather(this.state.jakarta.w)})</li>
+          <li><Link to="/detail/bandung">Bandung: </Link>{this.state.bandung.t} C ({this.formatWeather(this.state.bandung.w)})</li>
+          <li><Link to="/detail/surabaya">Surabaya: </Link>{this.state.surabaya.t} C ({this.formatWeather(this.state.surabaya.w)})</li>
         </ol>
       </div>
     );
