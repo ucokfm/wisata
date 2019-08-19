41661c1488cb7022007436337a4d00e4a53a8149
Ucok Freddy <ucokfm@gmail.com>
2019-08-19 15:37:30 +0700

    feat: show all cities

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,20 +5,36 @@ import { xml2js } from 'xml-js';
 interface Props {}
 
 export class Home extends React.Component<Props> {
+  state = {
+    denpasar: '0',
+    medan: '0',
+    jakarta: '0',
+    bandung: '0',
+    surabaya: '0',
+  }
+
   async componentDidMount() {
     const url = 'http://data.bmkg.go.id/datamkg/MEWS/DigitalForecast/DigitalForecast-Bali.xml';
     const result = await axios.get(url);
     const jsonData = xml2js(result.data);
     // console.log({ jsonData });
-    const city = jsonData["elements"][0]["elements"][0]["elements"][3]["attributes"].description;
+    const city: string = jsonData["elements"][0]["elements"][0]["elements"][3]["attributes"].description;
     const temperature = jsonData["elements"][0]["elements"][0]["elements"][3]["elements"][7]["elements"][0]["elements"][0]["elements"][0].text;
     console.log({ city, temperature });
+    this.setState({ [city.toLowerCase()]: temperature });
   }
 
   render() {
     return (
       <div>
         <h1>Home</h1>
+        <ol>
+          <li>Denpasar: {this.state.denpasar} C</li>
+          <li>Medan: {this.state.medan} C</li>
+          <li>Jakarta: {this.state.jakarta} C</li>
+          <li>Bandung: {this.state.bandung} C</li>
+          <li>Surabaya: {this.state.surabaya} C</li>
+        </ol>
       </div>
     );
   }
